9a2fedfe4f4fcee003b761f890a6c61c77fc0b5b
Dudi Zimberknopf <zimber.dudi@gmail.com>
2025-09-29 11:29:32 +0300

    undo logging

--- dt-tests/tests/test_runner/redis_test_util.rs
+++ dt-tests/tests/test_runner/redis_test_util.rs
@@ -116,12 +116,9 @@ impl RedisTestUtil {
         }
     }
 
-    pub fn execute_cmd(&self, conn: &mut Connection, cmd: &str) -> Value {
-        println!("execute cmd: {:?}", cmd);
+    pub fn execute_cmd(&self, conn: &mut Connection, cmd: &str) -> Value {        println!("execute cmd: {:?}", cmd);
         let packed_cmd = self.pack_cmd(cmd);
-        let res = conn.req_packed_command(&packed_cmd).unwrap();
-        println!("cmd result: {:?}", res);
-        res
+        conn.req_packed_command(&packed_cmd).unwrap()
     }
 
     pub fn execute_cmds_in_cluster(&self, conn: &mut RedisClusterConnection, cmds: &[String]) {
