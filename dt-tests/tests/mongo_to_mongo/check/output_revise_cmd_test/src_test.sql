f0e1e4cf0428a7a61e023429e423387e01824e03
loomt <loomt_@outlook.com>
2025-11-18 11:51:18 +0800

    add test and try to revise mongodb

---
+++ dt-tests/tests/mongo_to_mongo/check/output_revise_cmd_test/src_test.sql
@@ -0,0 +1,5 @@
+use test_db_1
+
+db.users.insertOne({ "name": "Alice", "age": 30, "email": "alice@example.com", "_id": "65733a82fb2ce9836745de01" });
+db.users.insertOne({ "name": "Bob", "age": 25, "email": "bob@example.com", "_id": "65733a82fb2ce9836745de02" });
+db.users.insertOne({ "name": "Charlie", "age": 35, "email": "charlie@example.com", "_id": "65733a82fb2ce9836745de03" });
