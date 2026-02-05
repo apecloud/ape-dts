74e03a4872fb9a9af0debef9acd1c8f1e004ff4d
Qinyu Cai <qinyu.cai@infracreate.com>
2026-02-05 11:33:54 +0200

    fix: use notify in dtqueue (#466)

--- dt-common/src/meta/dt_queue.rs
+++ dt-common/src/meta/dt_queue.rs
@@ -1,5 +1,8 @@
-use std::sync::atomic::{AtomicU64, Ordering};
-use tokio::task::yield_now;
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    Arc,
+};
+use tokio::sync::Notify;
 
 use concurrent_queue::{ConcurrentQueue, PopError, PushError};
 
@@ -10,6 +13,7 @@ pub struct DtQueue {
     check_memory: bool,
     max_bytes: u64,
     cur_bytes: AtomicU64,
+    not_full: Arc<Notify>,
 }
 
 impl DtQueue {
@@ -19,6 +23,7 @@ impl DtQueue {
             max_bytes,
             check_memory: max_bytes > 0,
             cur_bytes: AtomicU64::new(0),
+            not_full: Arc::new(Notify::new()),
         }
     }
 
@@ -44,30 +49,23 @@ impl DtQueue {
 
     #[inline(always)]
     pub async fn push(&self, mut item: DtItem) -> anyhow::Result<()> {
-        while self.queue.is_full() {
-            yield_now().await;
-        }
-
-        if self.check_memory {
-            while self.cur_bytes.load(Ordering::Acquire) > self.max_bytes {
-                yield_now().await;
-            }
-        }
-        self.cur_bytes
-            .fetch_add(item.dt_data.get_data_size(), Ordering::Release);
-
+        let item_size = item.dt_data.get_data_size();
         loop {
-            let res = self.queue.push(item);
-            match res {
-                Ok(_) => break,
-                Err(PushError::Full(returned_item)) => {
-                    item = returned_item;
-                    yield_now().await;
+            if !self.queue.is_full() && !self.is_mem_full() {
+                let res = self.queue.push(item);
+                match res {
+                    Ok(_) => {
+                        self.cur_bytes.fetch_add(item_size, Ordering::Release);
+                        return Ok(());
+                    }
+                    Err(PushError::Full(returned_item)) => {
+                        item = returned_item;
+                    }
+                    Err(e) => return Err(e.into()),
                 }
-                Err(e) => return Err(e.into()),
             }
+            self.not_full.notified().await;
         }
-        Ok(())
     }
 
     #[inline(always)]
@@ -81,6 +79,17 @@ impl DtQueue {
                 .fetch_sub(item.dt_data.get_data_size(), Ordering::Release);
         }
 
+        self.not_full.notify_one();
+
         Ok(item)
+    }
+
+    #[inline(always)]
+    fn is_mem_full(&self) -> bool {
+        if self.check_memory {
+            self.cur_bytes.load(Ordering::Acquire) > self.max_bytes
+        } else {
+            false
+        }
     }
 }
