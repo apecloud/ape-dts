e82af732323df8679a4b4b4bdb306c66cd18696f
gnolong <2391353625@qq.com>
2026-03-23 17:37:58 +0800

    fmt

--- dt-task/src/task_runner.rs
+++ dt-task/src/task_runner.rs
@@ -11,7 +11,14 @@ use anyhow::{bail, Context};
 use chrono::Local;
 use log4rs::config::{Config, Deserializers, RawConfig};
 use tokio::{
-    fs::{File, metadata}, io::AsyncReadExt, runtime::Handle, select, sync::{Mutex, RwLock}, task::JoinSet, time::Duration, try_join
+    fs::{metadata, File},
+    io::AsyncReadExt,
+    runtime::Handle,
+    select,
+    sync::{Mutex, RwLock},
+    task::JoinSet,
+    time::Duration,
+    try_join,
 };
 
 use super::{
@@ -139,7 +146,10 @@ impl TaskRunner {
         self.init_log4rs().await?;
 
         let worker_thread_cnt = Handle::current().metrics().num_workers();
-        log_info!("ape-dts started with {} worker thread(s)", worker_thread_cnt);
+        log_info!(
+            "ape-dts started with {} worker thread(s)",
+            worker_thread_cnt
+        );
 
         panic::set_hook(Box::new(|panic_info| {
             let backtrace = std::backtrace::Backtrace::capture();
