fbf0379b52538f9c4ef52f2c12c05ff19ff5315a
Qinyu Cai <qinyu.cai@infracreate.com>
2026-07-14 11:36:44 +0800

    fix: idle pipeline cause high cpu (#535)

--- dt-common/src/meta/dt_queue.rs
+++ dt-common/src/meta/dt_queue.rs
@@ -2,9 +2,9 @@ use std::sync::{
     atomic::{AtomicU64, Ordering},
     Arc,
 };
-use tokio::sync::Notify;
 
 use concurrent_queue::{ConcurrentQueue, PopError, PushError};
+use tokio::{sync::Notify, time::timeout, time::Duration};
 
 use super::dt_data::DtItem;
 
@@ -13,6 +13,7 @@ pub struct DtQueue {
     check_memory: bool,
     max_bytes: u64,
     cur_bytes: AtomicU64,
+    not_empty: Arc<Notify>,
     not_full: Arc<Notify>,
 }
 
@@ -23,6 +24,7 @@ impl DtQueue {
             max_bytes,
             check_memory: max_bytes > 0,
             cur_bytes: AtomicU64::new(0),
+            not_empty: Arc::new(Notify::new()),
             not_full: Arc::new(Notify::new()),
         }
     }
@@ -55,6 +57,7 @@ impl DtQueue {
                 match self.queue.push(item) {
                     Ok(_) => {
                         self.cur_bytes.fetch_add(item_size, Ordering::Release);
+                        self.not_empty.notify_one();
                         return Ok(());
                     }
                     Err(PushError::Full(returned_item)) => {
@@ -83,6 +86,10 @@ impl DtQueue {
         Ok(item)
     }
 
+    pub async fn wait_for_data(&self, max_wait: Duration) {
+        let _ = timeout(max_wait, self.not_empty.notified()).await;
+    }
+
     #[inline(always)]
     fn is_mem_full(&self) -> bool {
         if self.check_memory {
@@ -90,5 +97,41 @@ impl DtQueue {
         } else {
             false
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{sync::Arc, time::Duration};
+
+    use tokio::time::{sleep, timeout};
+
+    use super::DtQueue;
+    use crate::meta::dt_data::DtData;
+    use crate::meta::{dt_data::DtItem, position::Position};
+
+    #[tokio::test]
+    async fn wait_for_data_wakes_after_push() {
+        let queue = Arc::new(DtQueue::new(8, 0));
+        let waiter_queue = queue.clone();
+        let waiter = tokio::spawn(async move {
+            waiter_queue.wait_for_data(Duration::from_secs(30)).await;
+        });
+
+        sleep(Duration::from_millis(20)).await;
+        assert!(!waiter.is_finished());
+
+        queue
+            .push(DtItem {
+                dt_data: DtData::Heartbeat {},
+                position: Position::None,
+                data_origin_node: String::new(),
+            })
+            .await
+            .unwrap();
+        timeout(Duration::from_millis(200), waiter)
+            .await
+            .expect("waiter should wake after push")
+            .unwrap();
     }
 }
