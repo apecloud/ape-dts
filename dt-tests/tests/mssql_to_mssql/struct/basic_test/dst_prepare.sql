3ee537246bb40baa21fb14db464987aed321eaa2
gnolong <2391353625@qq.com>
2026-08-12 19:54:23 -0700

    test: expand SQL Server struct coverage

--- dt-tests/tests/mssql_to_mssql/struct/basic_test/dst_prepare.sql
+++ dt-tests/tests/mssql_to_mssql/struct/basic_test/dst_prepare.sql
@@ -1,3 +1,13 @@
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.full_column_type_view', N'V') IS NOT NULL
+    DROP VIEW struct_it_mssql2mssql_1.full_column_type_view;
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.[special_character_$1#@*_table]', N'U') IS NOT NULL
+    DROP TABLE struct_it_mssql2mssql_1.[special_character_$1#@*_table];
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.case_sensitive_column_name', N'U') IS NOT NULL
+    DROP TABLE struct_it_mssql2mssql_1.case_sensitive_column_name;
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.special_default_and_comment', N'U') IS NOT NULL
+    DROP TABLE struct_it_mssql2mssql_1.special_default_and_comment;
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.spatial_column_type', N'U') IS NOT NULL
+    DROP TABLE struct_it_mssql2mssql_1.spatial_column_type;
 IF OBJECT_ID(N'struct_it_mssql2mssql_1.constraint_table', N'U') IS NOT NULL
     DROP TABLE struct_it_mssql2mssql_1.constraint_table;
 IF OBJECT_ID(N'struct_it_mssql2mssql_1.full_index_type', N'U') IS NOT NULL
