e2193436f2131bd545361764734068234ade9c28
gnolong <2391353625@qq.com>
2026-08-18 02:22:35 -0700

    test: improve SQL Server struct coverage

--- dt-tests/tests/mssql_to_mssql/struct/basic_test/dst_prepare.sql
+++ dt-tests/tests/mssql_to_mssql/struct/basic_test/dst_prepare.sql
@@ -2,6 +2,16 @@ IF OBJECT_ID(N'struct_it_mssql2mssql_1.full_column_type_view', N'V') IS NOT NULL
     DROP VIEW struct_it_mssql2mssql_1.full_column_type_view;
 IF OBJECT_ID(N'struct_it_mssql2mssql_1.[special_character_$1#@*_table]', N'U') IS NOT NULL
     DROP TABLE struct_it_mssql2mssql_1.[special_character_$1#@*_table];
+IF EXISTS (
+    SELECT 1
+    FROM sys.tables AS t
+    JOIN sys.schemas AS s ON s.schema_id = t.schema_id
+    WHERE s.name = N'struct_it_mssql2mssql_1'
+      AND t.name = N'special_character_$1#@*].table'
+)
+    DROP TABLE struct_it_mssql2mssql_1.[special_character_$1#@*]].table];
+IF OBJECT_ID(N'struct_it_mssql2mssql_1.rowversion_type', N'U') IS NOT NULL
+    DROP TABLE struct_it_mssql2mssql_1.rowversion_type;
 IF OBJECT_ID(N'struct_it_mssql2mssql_1.case_sensitive_column_name', N'U') IS NOT NULL
     DROP TABLE struct_it_mssql2mssql_1.case_sensitive_column_name;
 IF OBJECT_ID(N'struct_it_mssql2mssql_1.special_default_and_comment', N'U') IS NOT NULL
