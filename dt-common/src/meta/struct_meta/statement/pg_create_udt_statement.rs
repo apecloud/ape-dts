0a39840c483334f19759ff96c6caa154ab597507
caiq1nyu <qinyu.cai@infracreate.com>
2026-09-04 11:58:06 +0800

    manual pick #583 from main and fix conflict

--- dt-common/src/meta/struct_meta/statement/pg_create_udt_statement.rs
+++ dt-common/src/meta/struct_meta/statement/pg_create_udt_statement.rs
@@ -1,7 +1,7 @@
+use crate::meta::struct_meta::statement::struct_statement::{StructKey, StructKeyType};
+use crate::meta::struct_meta::structure::structure_type::StructureType;
 use crate::meta::struct_meta::structure::user_defined::PgUdt;
 use crate::rdb_filter::RdbFilter;
-
-use crate::meta::struct_meta::structure::structure_type::StructureType;
 
 #[derive(Debug, Clone)]
 pub struct PgCreateUdtStatement {
@@ -9,18 +9,17 @@ pub struct PgCreateUdtStatement {
 }
 
 impl PgCreateUdtStatement {
-    pub fn route(&mut self, _dst_schema: &str) {
-        todo!("support route");
-    }
-
-    pub fn to_sqls(&self, filter: &RdbFilter) -> anyhow::Result<Vec<(String, String)>> {
+    pub fn to_sqls(&self, filter: &RdbFilter) -> anyhow::Result<Vec<(StructKey, String)>> {
         let mut sqls = Vec::new();
         if filter.filter_structure(&StructureType::Udt) {
             return Ok(sqls);
         }
 
         let sql = self.udt.create_statement.to_string();
-        let key = format!("udt.{}.{}", self.udt.schema_name, self.udt.typ_name);
+        let key = StructKey::new(
+            StructKeyType::Udt,
+            [&self.udt.schema_name, &self.udt.typ_name],
+        );
         sqls.push((key, sql));
         Ok(sqls)
     }
